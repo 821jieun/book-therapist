87e635e211a9f52f31e35d40674f630cb1ed50a6
Tudor Ilisoi <tudorilisoi@yahoo.com>
2018-04-05 21:26:03 +0300

    more spelling

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.post('/api/signup', inputParser, (req, res) => {
 
         // send HTTP status code 200 OK and finish processing this request
         // the status and code in the response are just fields I made up
-        // they have nothing to do with http status codes, this is the DSL of our API
+        // they have nothing to do with HTTP status codes, this is the DSL of our API
         res.json({ status: "OK" }, 200).end()
     }
     res.json({ status: "ERROR", code: "ALREADY_SIGNED_UP" }, 400).end()
