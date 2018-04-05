061329508f1cb22e7aba34440a6c69d6abcc6db2
Tudor Ilisoi <tudorilisoi@yahoo.com>
2018-04-05 21:13:13 +0300

    working mocked signup (API and client)

--- browser/js/signup.js
+++ browser/js/signup.js
@@ -1,40 +1,58 @@
 console.info('Signup JS, config is:', APP)
 
 const SIGNUP_FORM = $('#js-signup-form')
+const SIGNUP_MESSAGE_CONTAINER = $('#signup-message')
+
 const SIGNUP_URL = `${APP.API_URL}/signup`
 
-function apiSignup(formData, successCallback, errorCallback){
+function handleAPIResponse(response) {
+
+    //response has status and code fileds, see server.js
+    if (response.status === "OK") {
+        SIGNUP_MESSAGE_CONTAINER.html(
+        `<p>
+         Success! You may now <a href="/login.html">login</a>.
+        </p>
+        `)
+    } else {
+        SIGNUP_MESSAGE_CONTAINER.html(
+            `
+            <p>There was an error with code: ${response.code}</p>
+            <p>Hint: try username <strong>Jieun</strong> and password <strong>letmein</strong></p>    
+            `)
+    }
+}
+
+function apiSignup(formData, successCallback, errorCallback) {
     //POST to the server
     $.ajax({
-        dataType:'json',
-        method:'post',
-        url:SIGNUP_URL,
-        data:formData,
-        success:successCallback,
-        error:errorCallback,
+        dataType: 'json',
+        method: 'post',
+        url: SIGNUP_URL,
+        data: formData,
+        success: data => successCallback(data),
+        error: xhr => errorCallback(xhr.responseJSON),
     })
-}      
-        
+}
 
-function setupFormHandlers(){
-SIGNUP_FORM.submit( function(e){
-e.preventDefault()
 
-const ids = 'username-input password-input'.split(' ')
-let formData = {}
-ids.forEach(id=>{
-    let value = $(`#${id}`).val()
-    formData[id]=value
-})
+function setupFormHandlers() {
+    SIGNUP_FORM.submit(function(e) {
+        e.preventDefault()
 
-console.log('submit!', formData)
+        const ids = 'username-input password-input'.split(' ')
+        let formData = {}
+        ids.forEach(id => {
+            let value = $(`#${id}`).val()
+            const shortId = id.replace('-input', '')
+            formData[shortId] = value
+        })
 
-apiSignup(formData, (resp)=>{
-    console.log('Success!', resp)
-})
+        console.log('submit!', formData)
+        apiSignup(formData, handleAPIResponse, handleAPIResponse)
 
-return false
-})
+        return false
+    })
 
 }
 
--- server.js
+++ server.js
@@ -1,59 +1,75 @@
-
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(morgan('common'));
 app.use(express.static('browser'));
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/browser/home.html');
-// });
 
-app.get('/api/recommendations', (req, res) => {
-  // res.sendFile(__dirname + '/browser/home.html');
+
+// parse application/x-www-form-urlencoded as sent by our API consumers
+const inputParser = bodyParser.urlencoded({ extended: false })
+
+app.get('/api/recommendations', (inputParser, req, res) => {
+    // TODO
 });
-app.get('/api/login', (req, res) => {
-  // res.sendFile(__dirname + '/browser/login.html');
+app.get('/api/login', (inputParser, req, res) => {
+    // TODO
 });
-app.post('/api/signup', (req, res) => {
-  console.log('/api/signup:', req.body)
+app.post('/api/signup', inputParser, (req, res) => {
+    console.log('/api/signup:', req.body)
+
+    //extract body.username and body.password
+    const { username, password } = req.body
+    
+    //this is where the logic resides, checking wether input is valid and users does not already exist
+    //there may be more stuff to do, like sending an actication e-mail
+    
+    //TODO remove this mock
+    //TODO check for valid data and store the new user
+    if (username === 'Jieun' && password === 'letmein') {
+
+        //send HTTP status code 200 OK and finish processing this request
+        res.json({ status: "OK" }, 200).end()
+    }
+    res.json({ status: "ERROR", code: "ALREADY_SIGNED_UP" }, 400).end()
 
 });
 
 app.get('*', (req, res) => {
-  res.json('not found');
+    res.json('not found');
 });
 
 let server;
 
 function runServer() {
-  const port = process.env.PORT || 8080;
+    const port = process.env.PORT || 8080;
 
-  return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`listening attentively on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err);
+    return new Promise((resolve, reject) => {
+        server = app.listen(port, () => {
+            console.log(`listening attentively on port ${port}`);
+            resolve(server);
+        }).on('error', err => {
+            reject(err);
+        });
     });
-  });
 }
 
 function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log('Closing server');
-    server.close(err => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve();
+    return new Promise((resolve, reject) => {
+        console.log('Closing server');
+        server.close(err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        });
     });
-  });
 }
 
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
+    runServer().catch(err => console.error(err));
 };
 
-module.exports = { app, runServer, closeServer};
+module.exports = { app, runServer, closeServer };
