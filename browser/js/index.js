c2661843068c88168e7fb2211754120c5d808821
jieun <821jieun@gmail.com>
2018-05-17 18:28:25 -0400

    fixed text overlap in signup input fields

--- browser/js/index.js
+++ browser/js/index.js
@@ -1,6 +1,6 @@
 
-// const url = 'http://localhost:8080'
-const url = 'https://book-therapist.herokuapp.com'
+const url = 'http://localhost:8080'
+// const url = 'https://book-therapist.herokuapp.com'
 //
 
 window.addEventListener('resize', go);
@@ -17,6 +17,7 @@ function go(){
     $('.recent-recs').removeClass('displayNone');
   }
 }
+
 
 $(document).ready(function() {
   go();
